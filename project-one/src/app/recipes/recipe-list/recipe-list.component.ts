063861200a61d3e974c43ac94f8ee5b48d18ad4c
Eric Chin <ericychin94@gmail.com>
2022-08-07 03:52:52 -0700

    Section 3-55

--- project-one/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-one/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/62bf35ae872a6cfbb260f286/3:4/w_1800,h_2400,c_limit/0701-tj-recipe-potato-v2.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2020%2F03%2F19%2Ffideos-secos-tacos-FT-RECIPE0420-1.jpg&q=60'
+    ),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2020%2F03%2F19%2Ffideos-secos-tacos-FT-RECIPE0420-1.jpg&q=60')
   ];
 
   constructor() { }
