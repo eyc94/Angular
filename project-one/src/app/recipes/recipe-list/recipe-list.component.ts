22db4d5d30fe9b8322362a1fc67fad9cb02ecd6d
Eric Chin <ericychin94@gmail.com>
2022-08-07 03:44:49 -0700

    Section 3-54

--- project-one/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-one/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/62bf35ae872a6cfbb260f286/3:4/w_1800,h_2400,c_limit/0701-tj-recipe-potato-v2.jpg')
+  ];
 
   constructor() { }
 
