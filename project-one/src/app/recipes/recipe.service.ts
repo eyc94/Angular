d20ae588c16d2f69a9ff336ce04f0b45fcd0471e
Eric Chin <47379956+eyc94@users.noreply.github.com>
2022-08-08 16:47:53 -0700

    Section 10 (#12)
    * Section 10-117
    
    * Section 10-118
    
    * Section 10-119
    
    * Section 10-120
    
    * Section 10-121
    
    * Section 10-122
    
    * Section 10-123

---
+++ project-one/src/app/recipes/recipe.service.ts
@@ -0,0 +1,41 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Al Pastor Tacos',
+      'This is a picture of delicious tacos.',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2020%2F03%2F19%2Ffideos-secos-tacos-FT-RECIPE0420-1.jpg&q=60',
+      [
+        new Ingredient('Pork', 1),
+        new Ingredient('Tortilla', 10)
+      ]
+    ),
+    new Recipe(
+      'Hamburger',
+      'This is a picture of an In-N-Out burger.',
+      'https://www.discoverlosangeles.com/sites/default/files/media/activities/in-n-out-double-double-animal-style_1.jpg?width=1600&height=1200&fit=crop&quality=78&auto=webp',
+      [
+        new Ingredient('Meet', 2),
+        new Ingredient('Cheese', 2),
+        new Ingredient('Buns', 2)
+      ]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService) { }
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
