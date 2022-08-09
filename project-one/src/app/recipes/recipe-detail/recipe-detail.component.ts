f2a7e81e9afddf2dfd3fd5e664494d3d2e818976
Eric Chin <ericychin94@gmail.com>
2022-08-09 02:43:08 -0700

    Section 12-163

--- project-one/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ project-one/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -10,7 +10,7 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
-  id; number;
+  id: number;
 
   constructor(
     private recipeService: RecipeService,
