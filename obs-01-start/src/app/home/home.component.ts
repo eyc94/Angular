a3f4b6409016f61cc10337af210e78ab0e8fcfb7
Eric Chin <ericychin94@gmail.com>
2022-08-09 17:37:35 -0700

    Section 13-175

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -22,6 +22,12 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
+        if (count === 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
         count++;
       }, 1000);
     });
@@ -29,6 +35,13 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.firstObsSubscription = customIntervalObservable.subscribe(
       data => {
         console.log(data);
+      },
+      error => {
+        // console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed!');
       }
     );
   }
