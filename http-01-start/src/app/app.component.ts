9bc5bba06ac248879f6bb6ae8e1af088d775cecb
Eric Chin <ericychin94@gmail.com>
2022-08-11 16:02:40 -0700

    Section 18-258

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -35,10 +36,21 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://angular-test-ec-default-rtdb.firebaseio.com/posts.json').subscribe(
-      posts => {
-        console.log(posts);
-      }
-    )
+    this.http
+      .get('https://angular-test-ec-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], id: key });
+          }
+        }
+        return postsArray;
+      }))
+      .subscribe(
+        posts => {
+          console.log(posts);
+        }
+      );
   }
 }
