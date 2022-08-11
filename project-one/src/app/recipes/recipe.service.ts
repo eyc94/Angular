2cfd71710827846e14512052f06da0ede3e83251
Eric Chin <47379956+eyc94@users.noreply.github.com>
2022-08-11 02:37:18 -0700

    Section 16 (#18)
    * Section 16-219
    
    * Section 16-220
    
    * Section 16-221
    
    * Section 16-222
    
    * Section 16-223
    
    * Section 16-224
    
    * Section 16-225
    
    * Section 16-226
    
    * Section 16-227
    
    * Section 16-228
    
    * Section 16-229
    
    * Section 16-231
    
    * Section 16-232
    
    * Section 16-233
    
    * Section 16-234
    
    * Section 16-235
    
    * Section 16-236
    
    * Section 16-237
    
    * Section 16-238
    
    * Section 16-239

--- project-one/src/app/recipes/recipe.service.ts
+++ project-one/src/app/recipes/recipe.service.ts
@@ -6,6 +6,7 @@ import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -41,5 +42,20 @@ export class RecipeService {
 
   addIngredientToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
