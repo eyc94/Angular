e42859de56e9210dbacd4c47d48d37eac892d940
Eric Chin <ericychin94@gmail.com>
2022-08-11 16:25:15 -0700

    Section 18-261

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -38,6 +39,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>('https://angular-test-ec-default-rtdb.firebaseio.com/posts.json')
       .pipe(map(responseData => {
@@ -51,6 +53,7 @@ export class AppComponent implements OnInit {
       }))
       .subscribe(
         posts => {
+          this.isFetching = false;
           this.loadedPosts = posts;
         }
       );
