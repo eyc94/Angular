4a19526fe9c34f6e617506b643c6c9e3197f1634
Eric Chin <ericychin94@gmail.com>
2022-08-11 18:01:05 -0700

    Section 18-270

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map, catchError } from "rxjs/operators";
@@ -25,13 +25,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
       .get<{ [key: string]: Post }>(
         'https://angular-test-ec-default-rtdb.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({
-            "Custom-Header": "Hello"
-          })
+          headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+          params: searchParams
         }
       )
       .pipe(
