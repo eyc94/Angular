cf2cc591e6960c207051e7a82506e6c202a68b2c
Eric Chin <ericychin94@gmail.com>
2022-08-11 17:56:26 -0700

    Section 18-269

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Post } from "./post.model";
 import { map, catchError } from "rxjs/operators";
@@ -26,7 +26,14 @@ export class PostsService {
 
   fetchPosts() {
     return this.http
-      .get<{ [key: string]: Post }>('https://angular-test-ec-default-rtdb.firebaseio.com/posts.json')
+      .get<{ [key: string]: Post }>(
+        'https://angular-test-ec-default-rtdb.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({
+            "Custom-Header": "Hello"
+          })
+        }
+      )
       .pipe(
         map(responseData => {
           const postsArray: Post[] = [];
