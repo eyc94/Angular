0f6c5859b00ca27a0fbd6749a3e3744932695242
Eric Chin <ericychin94@gmail.com>
2022-08-14 00:19:44 -0700

    Section 20-298

--- project-one/src/app/auth/auth.service.ts
+++ project-one/src/app/auth/auth.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -39,5 +40,16 @@ export class AuthService {
       }
       return throwError(errorMessage);
     }));
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB0F_H7xKaUfWnUlr6H7ozOPHxdj52OEw8',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    );
   }
 }
